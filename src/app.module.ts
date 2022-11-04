a88deebb7c8d01f67c3c1b91fc92db910df86393
certong <tongtong0507@live.com>
2022-11-04 17:56:11 +0800

    + Create cats module for mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'my-nest-db', // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-    }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
     CoffeesModule,
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
