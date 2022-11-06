f6a3b3c98c5ea7c8913862cee87af05396a0a43b
certong <tongtong0507@live.com>
2022-11-06 21:44:41 +0800

    + Implement mongoose module

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -25,18 +25,18 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: string) {
     return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() creatCoffeeDto: CreateCoffeeDto) {
-    this.coffeesService.create(creatCoffeeDto)
+    this.coffeesService.create(creatCoffeeDto);
     return creatCoffeeDto;
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,48 +1,93 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'certong',
-      brand: 'Apple',
-      flavors: ['AAA', 'BBB'],
-    },
-  ];
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+  ) {}
 
   findAll() {
-    return this.coffees;
+    return this.coffeeModel.find().exec();
   }
 
-  findOne(id: number) {
-    const coffee = this.coffees.find((i) => i.id === id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
 
     if (!coffee) {
-      throw new NotFoundException(`Coffee #${id} not found`);
+      throw new NotFoundException(`Coffee #id ${id} not found`);
     }
 
     return coffee;
   }
 
-  update(id: number, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = new this.coffeeModel(createCoffeeDto);
 
-    if (existingCoffee) {
-      // Update coffee here
+    return coffee.save();
+  }
+
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.coffeeModel
+      .findByIdAndUpdate({ _id: id }, { $set: updateCoffeeDto }, { new: true })
+      .exec();
+
+    if (!existingCoffee) {
+      throw new NotFoundException(`Coffee #id ${id} not found`);
     }
+
+    return existingCoffee;
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
-  }
+  async remove(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((i) => i.id === +id);
-
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex);
-    }
+    return coffee.remove();
   }
+  // private coffees: Coffee[] = [
+  //   {
+  //     id: 1,
+  //     name: 'certong',
+  //     brand: 'Apple',
+  //     flavors: ['AAA', 'BBB'],
+  //   },
+  // ];
+
+  // findAll() {
+  //   return this.coffees;
+  // }
+
+  // findOne(id: number) {
+  //   const coffee = this.coffees.find((i) => i.id === id);
+
+  //   if (!coffee) {
+  //     throw new NotFoundException(`Coffee #${id} not found`);
+  //   }
+
+  //   return coffee;
+  // }
+
+  // update(id: number, updateCoffeeDto: any) {
+  //   const existingCoffee = this.findOne(id);
+
+  //   if (existingCoffee) {
+  //     // Update coffee here
+  //   }
+  // }
+
+  // create(createCoffeeDto: any) {
+  //   this.coffees.push(createCoffeeDto);
+  // }
+
+  // remove(id: string) {
+  //   const coffeeIndex = this.coffees.findIndex((i) => i.id === +id);
+
+  //   if (coffeeIndex >= 0) {
+  //     this.coffees.splice(coffeeIndex);
+  //   }
+  // }
 }
